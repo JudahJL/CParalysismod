fc1aec13b9a9217d6955961c340ab2e5e6697178
LeoneKingzz <167722980+LeoneKingzz@users.noreply.github.com>
2024-07-23 07:05:20 +0100

    dde

--- src/PCH.h
+++ src/PCH.h
@@ -6,3 +6,25 @@
 using namespace std::literals;
 
 namespace logger = SKSE::log;
+
+namespace Yen
+{
+    using namespace SKSE::stl;
+
+    template <class T>
+    void write_thunk_call(std::uintptr_t a_src)
+    {
+        auto &trampoline = SKSE::GetTrampoline();
+        SKSE::AllocTrampoline(14);
+        T::func = trampoline.write_call<5>(a_src, T::thunk);
+    }
+
+    template <class F, std::size_t idx, class T>
+    void write_vfunc()
+    {
+        REL::Relocation<std::uintptr_t> vtbl{F::VTABLE[0]};
+        T::func = vtbl.write_vfunc(idx, T::thunk);
+    }
+}
+
+#define OFFSET(se, ae) se
--- src/hook.cpp
+++ src/hook.cpp
@@ -1,121 +1,191 @@
 #include "hook.h"
 
-namespace hooks
+namespace ClassicParalysis
 {
-    RE::TESForm;
-    void on_animation_event::GetEquippedShout(RE::Actor *actor, bool SpellFire){
-        auto limboshout = actor->GetActorRuntimeData().selectedPower;
+    namespace detail
+    {
+        void freeze(RE::Actor &a_actor)
+        {
+            a_actor.PauseCurrentDialogue();
+            a_actor.InterruptCast(false);
+            a_actor.StopInteractingQuick(true);
 
-        if (limboshout && limboshout->Is(RE::FormType::Shout)){
-            auto data = RE::TESDataHandler::GetSingleton();
-            auto Hen = limboshout->As<RE::TESShout>()->variations->spell->GetKeywords();
-
-            for (RE::BGSKeyword* Yen : Hen){
-                std::string_view Lsht = (clib_util::editorID::get_editorID(Yen)).data();
-                switch (hash(Lsht.data(), Lsht.size())){
-                case "DSV_UnrelentingForce"_h:
-                    if (SpellFire){
-                        util::playSound(actor, (data->LookupForm<RE::BGSSoundDescriptorForm>(0x802, "DraugrShoutVoicing.esp")));
-                    }else{
-                        util::playSound(actor, (data->LookupForm<RE::BGSSoundDescriptorForm>(0x800, "DraugrShoutVoicing.esp")));
-                    }
-                    break;
-
-                case "DSV_Dismay"_h:
-                    if (SpellFire){
-                        util::playSound(actor, (data->LookupForm<RE::BGSSoundDescriptorForm>(0x805, "DraugrShoutVoicing.esp")));
-                    }else{
-                        util::playSound(actor, (data->LookupForm<RE::BGSSoundDescriptorForm>(0x804, "DraugrShoutVoicing.esp")));
-                    }
-                    break;
-
-                case "DSV_IceForm"_h:
-                    if (SpellFire){
-                        util::playSound(actor, (data->LookupForm<RE::BGSSoundDescriptorForm>(0x807, "DraugrShoutVoicing.esp")));
-                    }else{
-                        util::playSound(actor, (data->LookupForm<RE::BGSSoundDescriptorForm>(0x806, "DraugrShoutVoicing.esp")));
-                    }
-                    break;
-
-                case "DSV_Disarm"_h:
-                    if (SpellFire){
-                        util::playSound(actor, (data->LookupForm<RE::BGSSoundDescriptorForm>(0x809, "DraugrShoutVoicing.esp")));
-                    }else{
-                        util::playSound(actor, (data->LookupForm<RE::BGSSoundDescriptorForm>(0x808, "DraugrShoutVoicing.esp")));
-                    }
-                    break;
-
-                case "DSV_FrostBreath"_h:
-                    if (SpellFire){
-                        util::playSound(actor, (data->LookupForm<RE::BGSSoundDescriptorForm>(0x80B, "DraugrShoutVoicing.esp")));
-                    }else{
-                        util::playSound(actor, (data->LookupForm<RE::BGSSoundDescriptorForm>(0x80A, "DraugrShoutVoicing.esp")));
-                    }
-                    break;
-
-                case "DSV_PhantomForm"_h:
-                    if (SpellFire){
-                        util::playSound(actor, (data->LookupForm<RE::BGSSoundDescriptorForm>(0x80D, "DraugrShoutVoicing.esp")));
-                    }else{
-                        util::playSound(actor, (data->LookupForm<RE::BGSSoundDescriptorForm>(0x80C, "DraugrShoutVoicing.esp")));
-                    }
-                    break;
-
-                case "DSV_ElementalFury"_h:
-                    if (SpellFire){
-                        util::playSound(actor, (data->LookupForm<RE::BGSSoundDescriptorForm>(0x80F, "DraugrShoutVoicing.esp")));
-                    }else{
-                        util::playSound(actor, (data->LookupForm<RE::BGSSoundDescriptorForm>(0x80E, "DraugrShoutVoicing.esp")));
-                    }
-                    break;
-
-                case "DSV_BecomeEthereal"_h:
-                    if (SpellFire){
-                        util::playSound(actor, (data->LookupForm<RE::BGSSoundDescriptorForm>(0x811, "DraugrShoutVoicing.esp")));
-                    }else{
-                        util::playSound(actor, (data->LookupForm<RE::BGSSoundDescriptorForm>(0x810, "DraugrShoutVoicing.esp")));
-                    }
-                    break;
-
-                default:
-
-                    break;
-                }
+            if (const auto currentProcess = a_actor.GetActorRuntimeData().currentProcess)
+            {
+                currentProcess->ClearMuzzleFlashes();
             }
+
+            a_actor.GetActorRuntimeData().boolFlags.reset(RE::Actor::BOOL_FLAGS::kShouldAnimGraphUpdate);
+
+            if (const auto charController = a_actor.GetCharController(); charController)
+            {
+                charController->flags.set(RE::CHARACTER_FLAGS::kNotPushable);
+
+                charController->flags.reset(RE::CHARACTER_FLAGS::kRecordHits);
+                charController->flags.reset(RE::CHARACTER_FLAGS::kHitFlags);
+            }
+
+            a_actor.EnableAI(false);
+            a_actor.StopMoving(1.0f);
+        }
+
+        void unfreeze(RE::Actor &a_actor)
+        {
+            a_actor.GetActorRuntimeData().boolFlags.set(RE::Actor::BOOL_FLAGS::kShouldAnimGraphUpdate);
+
+            if (const auto charController = a_actor.GetCharController())
+            {
+                charController->flags.reset(RE::CHARACTER_FLAGS::kNotPushable);
+
+                charController->flags.set(RE::CHARACTER_FLAGS::kRecordHits);
+                charController->flags.set(RE::CHARACTER_FLAGS::kHitFlags);
+            }
+
+            a_actor.EnableAI(true);
         }
     }
 
+    namespace Effect
+    {
+        bool is_invalid_effect(RE::ParalysisEffect *a_this)
+        {
+            if (const auto spell = a_this->spell)
+            {
+                static auto frostKeyword = RE::TESForm::LookupByEditorID<RE::BGSKeyword>("MagicDamageFrost");
 
-	void on_animation_event::ProcessEvent(RE::BSTEventSink<RE::BSAnimationGraphEvent>* a_sink, RE::BSAnimationGraphEvent* a_event, RE::BSTEventSource<RE::BSAnimationGraphEvent>* a_eventSource)
-	{
-		if (!a_event->holder) {
-			return;
-		}
-		std::string_view eventTag = a_event->tag.data();
-		RE::Actor* actor = const_cast<RE::TESObjectREFR*>(a_event->holder)->As<RE::Actor>();
-        auto data = RE::TESDataHandler::GetSingleton();
-        if(actor->GetActorBase()->GetVoiceType() == data->LookupForm<RE::BGSVoiceType>(0x1F1CD, "Skyrim.esm")){
-            switch (hash(eventTag.data(), eventTag.size())){
-            case "BeginCastVoice"_h:
-
-                GetEquippedShout(actor);
-
-                break;
-
-            case "Voice_SpellFire_Event"_h:
-
-                GetEquippedShout(actor, true);
-
-                break;
+                const auto settings = Settings::GetSingleton();
+                if (spell->HasKeyword(frostKeyword) && !settings->frostSpells || !settings->paralysisSpells)
+                {
+                    return true;
+                }
             }
+            return false;
         }
-        
-	}
 
-	EventResult on_animation_event::ProcessEvent_NPC(RE::BSTEventSink<RE::BSAnimationGraphEvent>* a_sink, RE::BSAnimationGraphEvent* a_event, RE::BSTEventSource<RE::BSAnimationGraphEvent>* a_eventSource)
-	{
-		ProcessEvent(a_sink, a_event, a_eventSource);
-		return _ProcessEvent_NPC(a_sink, a_event, a_eventSource);
-	}
+        struct Start
+        {
+            static void thunk(RE::ParalysisEffect *a_this)
+            {
+                if (is_invalid_effect(a_this))
+                {
+                    return func(a_this);
+                }
 
+                const auto target = a_this->target;
+                const auto ref = target ? target->GetTargetStatsObject() : nullptr;
+                const auto actor = ref ? ref->As<RE::Actor>() : nullptr;
+
+                if (actor)
+                {
+                    if (actor->GetFormID() == SaadiaID)
+                    {
+                        func(a_this);
+                    }
+                    else if (!actor->IsDead())
+                    {
+                        if (!actor->IsPlayerRef())
+                        {
+                            detail::freeze(*actor);
+                        }
+                        else
+                        {
+                            actor->SetLifeState(RE::ACTOR_LIFE_STATE::kRestrained);
+                        }
+                    }
+                }
+            }
+            static inline REL::Relocation<decltype(thunk)> func;
+        };
+
+        struct Finish
+        {
+            static void thunk(RE::ParalysisEffect *a_this)
+            {
+                if (is_invalid_effect(a_this))
+                {
+                    return func(a_this);
+                }
+
+                const auto target = a_this->target;
+                const auto ref = target ? target->GetTargetStatsObject() : nullptr;
+                const auto actor = ref ? ref->As<RE::Actor>() : nullptr;
+
+                if (actor)
+                {
+                    if (actor->GetFormID() == SaadiaID)
+                    {
+                        func(a_this);
+                    }
+                    else
+                    {
+                        if (!actor->IsPlayerRef())
+                        {
+                            detail::unfreeze(*actor);
+                        }
+                        else
+                        {
+                            actor->SetLifeState(RE::ACTOR_LIFE_STATE::kAlive);
+                        }
+                        actor->StopMoving(1.0f);
+                    }
+                }
+            }
+            static inline REL::Relocation<decltype(thunk)> func;
+        };
+
+        void Install()
+        {
+            Yen::write_vfunc<RE::ParalysisEffect, 0x14, Start>();
+            Yen::write_vfunc<RE::ParalysisEffect, 0x15, Finish>();
+        }
+    }
+
+    namespace Fixes
+    {
+        struct CanBePushed
+        {
+            static bool thunk(RE::Actor *a_actor)
+            {
+                const auto result = func(a_actor);
+                if (result && a_actor && !a_actor->IsAIEnabled())
+                {
+                    detail::unfreeze(*a_actor);
+                }
+                return result;
+            }
+            static inline REL::Relocation<decltype(thunk)> func;
+        };
+
+        struct GetProcessLevel
+        {
+            static std::int32_t thunk(RE::Actor *a_actor)
+            {
+                const auto processLevel = func(a_actor);
+                if (a_actor && !a_actor->IsAIEnabled())
+                {
+                    detail::unfreeze(*a_actor);
+                    return -1;
+                }
+                return processLevel;
+            }
+            static inline REL::Relocation<decltype(&thunk)> func;
+        };
+
+        void Install()
+        {
+            REL::Relocation<std::uintptr_t> push_actor_away{RELOCATION_ID(38858, 39895), OFFSET(0x7E, 0x68)};
+            Yen::write_thunk_call<CanBePushed>(push_actor_away.address());
+
+            REL::Relocation<std::uintptr_t> start_kill_move{RELOCATION_ID(37659, 38613), 0x10};
+            Yen::write_thunk_call<GetProcessLevel>(start_kill_move.address());
+        }
+    }
+
+    void Install()
+    {
+        Settings::GetSingleton()->Load();
+
+        Effect::Install();
+        Fixes::Install();
+    }
 }
